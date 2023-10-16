79dce0ae57ae4798aee740cf2c2bfa6e417ece8c
Richard Tsibu Darkoh <paakwesi5976@gmail.com>
2023-10-16 15:00:04 -0700

    this is the invoking function

--- my_printf.c
+++ my_printf.c
@@ -22,7 +22,7 @@ int _printf(const char *format, ...)
 			index++;
 			eval = format[index];
 			if (eval == '\0')
-				return (-1);
+				break;
 			else if (eval == 'c' || eval == 's' || eval == '%'
 					|| eval == 'd' || eval == 'i')
 				my_length += f_caller(eval)(printf_arg);
