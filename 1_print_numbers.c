a9dc0d182e685033a1716f17b626d4eb23b908a0
Ramla-Ramadane <ramlaramadane@gmail.com>
2023-10-16 09:55:25 -0700

    function that prints integers

--- 1_print_numbers.c
+++ 1_print_numbers.c
@@ -1,4 +1,29 @@
 #include "main.h"
+#include <stdio.h>
+
+void _convert(int c)
+{
+	if (c < 0)
+	{
+		putchar('-');
+		c = -c;
+	}
+	if (c == 0)
+	{
+		putchar('0');
+		return;
+	}
+	if (c < 10)
+	{
+		putchar(c + '0');
+		return;
+	}
+	if (c >= 10)
+	{
+	_convert(c/10);
+	putchar((c%10) + '0');
+	}
+}
 
 /**
  * _printf - function that produces output according to a format
@@ -7,36 +32,42 @@
  */
 int _printf(const char *format, ...)
 {
+	int print = 0;
 	va_list print_integers;
-
-	va_start(print_integers, format);
+	
 	if (format == NULL)
 	{
 		return (-1);
 	}
+
+	va_start(print_integers, format);
 	while (*format)
 	{
 		if (*format == '%')
 		{
 			format++;
 		}
-		else
-		{
-			_putchar(*format);
-		}
 		if (*format == 'd')
 		{
-			int print = va_arg(print_integers, int);
-			_putchar(print);
+			int print_number = va_arg(print_integers, int);
+			_convert(print_number);
+			print++;
 		}
 		else if (*format == 'i')
 		{
 			int calculate = va_arg(print_integers, int);
-			_putchar(calculate);
+			_convert(calculate);
+			print++;
 		}
+		else
+		{
+		putchar(*format);
+		print++;
+		}
+		
 		format++;
 	}
-	return (*format);
 	va_end(print_integers);
+	return (print);
 }
-
+			
